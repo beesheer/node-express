c9d1b47e5703f65f1871eba58d0366908c4e026b
Bin Xu <bxu@franchiseblast.com>
2018-10-12 16:32:39 -0400

    node-express assignment 1 task 2

--- node-express/index.js
+++ node-express/index.js
@@ -2,12 +2,14 @@ const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
 
 const server = http.createServer(app);
 
