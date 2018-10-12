eb863f434e3029d5824d0d608ff9ff8b47857940
Bin Xu <bxu@franchiseblast.com>
2018-10-12 16:14:31 -0400

    Add bodyparser.

--- node-express/index.js
+++ node-express/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
